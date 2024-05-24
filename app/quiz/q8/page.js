0f47a4d804bfde8e1ee8c25765000156e688ee90
chubu <moomost123@gmail.com>
2024-05-24 21:41:26 +0700

    fix bg

--- app/quiz/q8/page.js
+++ app/quiz/q8/page.js
@@ -2,7 +2,6 @@
 import Container from "@/app/components/Container";
 import React, { useState } from "react";
 import "animate.css";
-import "../../components/changeBg.css"
 import { useRouter } from "next/navigation";
 
 const Q7 = () => {
